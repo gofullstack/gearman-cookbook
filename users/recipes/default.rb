69822a861243be05faa60df03f663c85840b18f3
Nathan L Smith <nlloyds@gmail.com>
2010-08-06 11:22:19 -0500

    lock root user

--- users/recipes/default.rb
+++ users/recipes/default.rb
@@ -23,6 +23,11 @@ search(:users, 'groups:admin') do |u|
 
   home_dir = "/home/#{u['id']}"
 
+  # Lock the root user
+  user "root" do
+    action :lock
+  end
+
   user u['id'] do
     uid u['uid']
     shell u['shell']
