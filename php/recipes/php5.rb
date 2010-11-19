72e2be5c8aa7750f5ca093d45472716a3f12a3e6
Nathan L Smith <nlloyds@gmail.com>
2010-11-19 14:10:25 -0600

    php

--- php/recipes/php5.rb
+++ php/recipes/php5.rb
@@ -19,11 +19,12 @@
 #
 
 include_recipe "apache2"
+include_recipe "php::module_curl"
 include_recipe "php::module_mysql"
-include_recipe "php::module_ldap"
-include_recipe "php::module_memcache"
-include_recipe "php::module_gd"
-include_recipe "php::module_pgsql"
+#include_recipe "php::module_ldap"
+#include_recipe "php::module_memcache"
+#include_recipe "php::module_gd"
+#include_recipe "php::module_pgsql"
 include_recipe "php::pear"
 
 cookbook_file value_for_platform([ "centos", "redhat", "fedora", "suse" ] => {"default" => "/etc/php.ini"}, "default" => "/etc/php5/apache2/php.ini") do
