f07a10e70a7abf6c6417f2872a7108ffd256290a
Nathan L Smith <nlloyds@gmail.com>
2010-12-01 10:54:02 -0600

    db/git config for gearman server

--- gearman/recipes/server.rb
+++ gearman/recipes/server.rb
@@ -9,6 +9,7 @@
 
 include_recipe "postgresql::server"
 
+# Create user
 user "gearman" do
   shell "/bin/bash"
   action :create
@@ -20,18 +21,36 @@ group "gearman" do
   action :create
 end
 
-# FIXME: Can't check this out since user doesn't have repo access.
-#        Just create the dir for now
-directory node[:gearman][:server][:path] do
-#git node[:gearman][:server][:path] do
-  #repository node[:gearman][:server][:repo]
-  #user "gearman"
-  group "gearman"
-  #action :sync
+# Add deploy keys
+directory "/home/gearman/.ssh" do
   owner "gearman"
-  mode "0755"
+  group "gearman"
+  mode "0700"
 end
 
+cookbook_file "/home/gearman/.ssh/id_rsa" do
+  backup false
+  owner "gearman"
+  group "gearman"
+  mode "0600"
+end
+
+cookbook_file "/home/gearman/.ssh/id_rsa.pub" do
+  backup false
+  owner "gearman"
+  group "gearman"
+  mode "0600"
+end
+
+# Git clone
+git node[:gearman][:server][:path] do
+  repository node[:gearman][:server][:repo]
+  user "gearman"
+  group "gearman"
+  action :sync
+end
+
+# Database config
 # TODO: Configure database with attributes
 template node[:gearman][:server][:path] + "/config/database.yml" do
   source "database.yml.erb"
@@ -40,6 +59,7 @@ template node[:gearman][:server][:path] + "/config/database.yml" do
   mode "0644"
 end
 
+# Bundle
 bash "bundle install" do
   cwd node[:gearman][:server][:path]
   code "bundle install --deployment"
